38c8779e2f83ef5e86cfaa78366769f9e76e121d
jshemas <jshemas@hotmail.com>
2015-01-13 10:23:14 -0500

    adding colors!

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var keypress = require('keypress'),
-	fs = require('fs');
+	fs = require('fs'),
+	clc = require('cli-color');
 
 // make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
@@ -8,6 +9,7 @@ keypress(process.stdin);
 var codeBlock,
 	codeBlockPlaceBefore = 0
 	codeBlockPlaceAfter = 5;
+
 fs.readFile( __dirname + '/code.txt', function (err, data) {
 	if (err) {
 		throw err;
@@ -15,11 +17,17 @@ fs.readFile( __dirname + '/code.txt', function (err, data) {
 	codeBlock = data.toString();
 });
 
+// set out colors
+var msg = clc.xterm(47).bgXterm(0);
+
+process.stdout.write(msg('\u001B[2J\u001B[0;0f'));
+
+// listen for inputs
 process.stdin.on('keypress', function (ch, key) {
 	if (key && key.ctrl && key.name == 'c') {
 		process.stdin.pause();
 	};
-	process.stdout.write(codeBlock.substring(codeBlockPlaceBefore, codeBlockPlaceAfter));
+	process.stdout.write(msg(codeBlock.substring(codeBlockPlaceBefore, codeBlockPlaceAfter)));
 	codeBlockPlaceBefore = codeBlockPlaceBefore + 5;
 	codeBlockPlaceAfter = codeBlockPlaceAfter + 5;
 	if (codeBlock.substring(codeBlockPlaceBefore, codeBlockPlaceAfter).indexOf('};') != -1) {
