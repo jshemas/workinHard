ef5484378c1f80a8834372c53cb4becdef991cd0
jshemas <joshshemas@gmail.com>
2017-10-03 18:36:50 -0400

    update codebase

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 #!/usr/bin/env node
 
-var keypress = require('keypress'),
-	fs = require('fs'),
-	clc = require('cli-color');
+var keypress = require('keypress');
+var fs = require('fs');
+var clc = require('cli-color');
 
 // make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
 
 // import code outputs
-var codeBlock,
-	codeBlockPlaceBefore = 0
-	codeBlockPlaceAfter = 5;
+var codeBlock;
+var codeBlockPlaceBefore = 0;
+var codeBlockPlaceAfter = 5;
 
 // if code choice is made, use that
 if (process.argv.slice(2).length !== 0) {
-	var fileName = process.argv.slice(2)[0];
-	fileName = fileName.replace(/-/g, '');
-	fs.readFile( __dirname + '/code/' + fileName + '.txt', function (err, data) {
-		if (err) {
-			throw err;
-		}
-		codeBlock = data.toString();
-	});
+  var fileName = process.argv.slice(2)[0];
+  fileName = fileName.replace(/-/g, '');
+  fs.readFile(__dirname + '/code/' + fileName + '.txt', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    codeBlock = data.toString();
+  });
 } else {
-	fs.readFile( __dirname + '/code/code.txt', function (err, data) {
-		if (err) {
-			throw err;
-		}
-		codeBlock = data.toString();
-	});
+  fs.readFile(__dirname + '/code/code.txt', function (err, data) {
+    if (err) {
+      throw err;
+    }
+    codeBlock = data.toString();
+  });
 }
 
 // set out colors
@@ -38,18 +38,18 @@ process.stdout.write(msg('\u001B[2J\u001B[0;0f'));
 
 // listen for inputs
 process.stdin.on('keypress', function (ch, key) {
-	if (key && key.ctrl && key.name === 'c') {
-		msg = clc.xterm(0).bgXterm(231);
-		process.stdout.write(msg('\u001B[2J\u001B[0;0f'));
-		process.stdin.pause();
-	};
-	process.stdout.write(msg(codeBlock.substring(codeBlockPlaceBefore, codeBlockPlaceAfter)));
-	codeBlockPlaceBefore = codeBlockPlaceBefore + 5;
-	codeBlockPlaceAfter = codeBlockPlaceAfter + 5;
-	if (codeBlock.substring(codeBlockPlaceBefore, codeBlockPlaceAfter).indexOf('};') !== -1) {
-		codeBlockPlaceBefore = 0
-		codeBlockPlaceAfter = 5;
-	}
+  if (key && key.ctrl && key.name === 'c') {
+    msg = clc.xterm(0).bgXterm(231);
+    process.stdout.write(msg('\u001B[2J\u001B[0;0f'));
+    process.stdin.pause();
+  };
+  process.stdout.write(msg(codeBlock.substring(codeBlockPlaceBefore, codeBlockPlaceAfter)));
+  codeBlockPlaceBefore = codeBlockPlaceBefore + 5;
+  codeBlockPlaceAfter = codeBlockPlaceAfter + 5;
+  if (codeBlock.substring(codeBlockPlaceBefore, codeBlockPlaceAfter).indexOf('};') !== -1) {
+    codeBlockPlaceBefore = 0;
+    codeBlockPlaceAfter = 5;
+  }
 });
 
 process.stdin.setRawMode(true);
